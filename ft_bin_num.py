4f114e6fd94c9a14568d93cb32f3635d02efd0d5
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 18:57:31 +0300

    Update ft_bin_num.py

--- ft_bin_num.py
+++ ft_bin_num.py
@@ -1,7 +1,15 @@
 def ft_bin_num(a):
+    b = 0
+    l = 0
     c = 0
+    m = a
     if a >= 0:
         while a > 0:
-            c = c * 10 + a % 2
-            a = a // 2
+            l = l + 1
+            a = a // 10
+    if a >= 0:
+        while m > 0:
+            c = (m % 2) * 10 ** l
+            m = m // 2
+            l = l - 1
         return c
