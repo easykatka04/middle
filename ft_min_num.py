c8ed2930f87a8a49a3f4e3ce37318b9845758eef
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 17:20:46 +0300

    Add files via upload

--- ft_min_num.py
+++ ft_min_num.py
@@ -2,13 +2,13 @@ def ft_min_num(a):
     l = 10
     c = 0
     if a >= 0:
-        while a > 0:
+        while a > 0 and a % 1 == 0:
             b = a % 10
             if b < l:
                 l = b
             a = a // 10
         return l
-    if a < 0:
+    if a < 0 and a % 1 == 0:
         a = a * -1
         while a > 0:
             b = a % 10
