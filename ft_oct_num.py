8a130107e41916847ae3f9fadcdfb46a5612fb9e
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 21:59:29 +0300

    Update ft_oct_num.py

--- ft_oct_num.py
+++ ft_oct_num.py
@@ -1,7 +1,8 @@
 def ft_oct_num(a):
-    c = 0
-    if a >= 0:
-        while a > 0:
-            c = c * 10 + a % 8
-            a = a // 8
-        return c
+    b = 1
+    d = 0
+    while a > 0:
+        d += a % 8 * b
+        b *= 10
+        a //= 8
+    return d
