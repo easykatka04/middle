bb6d18e460d5bb84f6619a586f7f8eb74bbe0dc7
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 22:10:30 +0300

    Add files via upload

--- ft_rev_bin_num.py
+++ ft_rev_bin_num.py
@@ -1,11 +1,26 @@
+def ft_pow(a, s):
+    b = a
+    if s > 0:
+        for i in range(s - 1):
+            a *= b
+        return a
+    elif s == 0:
+        return 1
+    for i in range(a):
+        a /= b
+    return a
+
+
 def ft_rev_bin_num(a):
-    c = 0
-    l = 0
-    k = 1
-    c = c + (a % 10)
-    l = l + 1
-    while a > 0:
-        c = c + (a % 10 * k)
-        k = k * 2
-        a = a // 10
-    return c
+    c = a
+    v = 0
+    d = 0
+    while c > 0:
+        c //= 10
+        d += 1
+    for digit in range(d):
+        v += a % 10 * ft_pow(2, digit)
+        a //= 10
+    return v
+
+
