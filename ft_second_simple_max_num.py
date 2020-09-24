a496ea26adeb4576d24224bb65a5100bd79a748f
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 21:56:12 +0300

    Update ft_second_simple_max_num.py

--- ft_second_simple_max_num.py
+++ ft_second_simple_max_num.py
@@ -1,30 +1,16 @@
 def ft_second_simple_max_num(a):
+    d = 0
     b = 0
     c = a
-    l = 0
-    if a >= 0 and a % 1 == 0:
-        while a > 0:
-            if b < a % 10:
-                b = a % 10
-            a = a // 10
-    if a < 0 and a % 1 == 0:
-        a = a * -1
-        while a > 0:
-            if b < a % 10:
-                b = a % 10
-            a = a // 10
-    if c >= 0 and c % 1 == 0:
-        while c > 0:
-            if l < c % 10 and c % 10 != b:
-                l = c % 10
-            c = c // 10
-    if c < 0 and c % 1 == 0:
-        c = c * -1
-        while c > 0:
-            if l < c % 10 and c % 10 != b:
-                l = c % 10
-            c = c // 10
-    if l == 0:
+    while c > 0:
+        if d < c % 10:
+            d = c % 10
+        c //= 10
+
+    while a > 0:
+        if d >= b < a % 10:
+            b = a % 10
+        a //= 10
+
+    if b == d:
         return -1
-    if l != b:
-        return l
