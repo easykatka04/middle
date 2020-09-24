2e6b5d5de08388390cd3cbc7e9401ec55027420e
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 21:48:16 +0300

    Update ft_rev_num.py

--- ft_rev_num.py
+++ ft_rev_num.py
@@ -1,11 +1,11 @@
 def ft_rev_num(a):
     c = 0
-    if a >= 0 and a % 1 == 0:
+    if a >= 0:
         while a > 0:
             c = c * 10 + a % 10
             a = a // 10
         return c
-    if a < 0 and a % 1 == 0:
+    if a < 0:
         a = a * -1
         while a > 0:
             c = c * 10 + a % 10
