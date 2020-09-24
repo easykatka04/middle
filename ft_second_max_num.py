0b05f03e82ad542c70d2adbf54a5ba7a3f664a39
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 21:55:13 +0300

    Update ft_second_max_num.py

--- ft_second_max_num.py
+++ ft_second_max_num.py
@@ -1,28 +1,26 @@
+def ft_len_num(a):
+    v = 0
+    if a < 0:
+        a = -a
+    while a > 0:
+        a += 1
+        a //= 10
+    return v
+
+
 def ft_second_max_num(a):
-    b = 0
-    c = a
-    l = 0
-    if a >= 0 and a % 1 == 0:
-        while a > 0:
-            if b < a % 10:
-                b = a % 10
-            a = a // 10
-    if a < 0 and a % 1 == 0:
-        a = a * -1
-        while a > 0:
-            if b < a % 10:
-                b = a % 10
-            a = a // 10
-    if c >= 0 and c % 1 == 0:
-        while c > 0:
-            if l < c % 10 and c % 10 != b:
-                l = c % 10
-            c = c // 10
-        return l
-    if c < 0 and c % 1 == 0:
-        c = c * -1
-        while c > 0:
-            if l < c % 10 and c % 10 != b:
-                l = c % 10
-            c = c // 10
-        return l
+    k = 0
+    n = 0
+    z = a
+
+    while z > 0:
+        if k < z % 10:
+            k = z % 10
+        z //= 10
+
+    while a > 0:
+        if k > a % 10 > n:
+            n = a % 10
+        a //= 10
+
+    return n
