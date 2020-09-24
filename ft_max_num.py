c61047f8528e6767e1e5db6aaa9e372035f1c900
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-24 17:17:14 +0300

    Add files via upload

--- ft_max_num.py
+++ ft_max_num.py
@@ -1,12 +1,12 @@
 def ft_max_num(a):
-    if a >= 0:
+    if a >= 0 and a % 1 == 0:
         b = a % 10
         while a > 0:
             a = a // 10
             if b < a % 10:
                 b = a % 10
         return b
-    if a < 0:
+    if a < 0 and a % 1 == 0:
         a = a * -1
         b = a % 10
         while a > 0:
